f59e856a58e108c0b70844d811fe0cfe6b54ac84
SahrulRamadhani29 <ramabusiness2910@gmail.com>
2024-09-05 11:07:22 +0700

    Update biodata17Rayhan.java

--- biodata17Rayhan.java
+++ biodata17Rayhan.java
@@ -12,5 +12,16 @@ public class biodata17Rayhan {
     System.out.println("Alamat \t: Perumahan Kebonagung Residence Blok H-32");
     System.out.println("No HP \t: 085704914266");
     System.out.println("Hobi \t: Olahraga");
+        System.out.println("----------------------------------------------------------");
+    System.out.println("-- Biodata Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+    System.out.println("----------------------------------------------------------");
+    System.out.println("Nama \t: Sahrul Ramadhani");
+    System.out.println("NIM \t: 244107020058");
+    System.out.println("Kelas \t: TI 1H");
+    System.out.println("No Absen: 24");
+    System.out.println("Prodi \t: D-IV Teknik Informatika");
+    System.out.println("Alamat \t: Jalan Kembang Turi");
+    System.out.println("No HP \t: 085163632919");
+    System.out.println("Hobi \t: Renang");
     }
 }
