25d0244b3efc4008b29bba6a0f74d86049e1a077
muhammadrayhan11 <rayhanzmzmi03@gmail.com>
2024-09-05 10:57:58 +0700

    first commit

---
+++ biodata17Rayhan.java
@@ -0,0 +1,16 @@
+public class biodata17Rayhan {
+    
+    public static void main(String[] args) {
+    System.out.println("----------------------------------------------------------");
+    System.out.println("-- Biodata Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+    System.out.println("----------------------------------------------------------");
+    System.out.println("Nama \t: Muhammad Rayhan Zamzami");
+    System.out.println("NIM \t: 244107020027");
+    System.out.println("Kelas \t: TI 1H");
+    System.out.println("No Absen: 17");
+    System.out.println("Prodi \t: D-IV Teknik Informatika");
+    System.out.println("Alamat \t: Perumahan Kebonagung Residence Blok H-32");
+    System.out.println("No HP \t: 085704914266");
+    System.out.println("Hobi \t: Olahraga");
+    }
+}
